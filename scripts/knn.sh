6684540d4f82d8fed827a4eeac8c9bb1e52901fa
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-02-14 14:33:10 +0200

    adjusted path once more

--- scripts/knn.sh
+++ scripts/knn.sh
@@ -2,5 +2,5 @@
 #SBATCH -p main
 #SBATCH -n4
 
-singularity build --fakeroot torch_env.sif ./containers/torch_env.def
+singularity build --fakeroot ./containers/torch_env.sif ./containers/torch_env.def
 singularity exec torch_env.sif python train.py
