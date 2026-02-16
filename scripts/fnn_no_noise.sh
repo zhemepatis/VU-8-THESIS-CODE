5a1b14cfc98adaa15f0320951d7f67959ebd1694
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-02-16 10:27:11 +0200

    some adjustments for fnn experiment scripts

--- scripts/fnn_no_noise.sh
+++ scripts/fnn_no_noise.sh
@@ -3,13 +3,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 1000
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 1000
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 10000
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 10000
 
 singularity run ./containers/torch.sif fnn_experiment.py \
     --processes 10 \
@@ -17,8 +17,4 @@ singularity run ./containers/torch.sif fnn_experiment.py \
 
 singularity run ./containers/torch.sif fnn_experiment.py \
     --processes 10 \
-    --data-set-size 1000000 
-
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 10000000 
+    --data-set-size 1000000
