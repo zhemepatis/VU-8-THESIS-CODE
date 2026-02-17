5c983c4778bd033d225c951925580ddf5ab4293f
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-02-17 16:32:00 +0200

    more cores and more threads

--- scripts/fnn_no_noise.sh
+++ scripts/fnn_no_noise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p main
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=20
 
 # singularity run ./containers/torch.sif fnn_experiment.py \
 #     --processes 10 \
@@ -12,7 +12,7 @@
 #     --data-set-size 10000
 
 singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 5 \
+    --processes 10 \
     --data-set-size 100000
 
 # singularity run ./containers/torch.sif fnn_experiment.py \
