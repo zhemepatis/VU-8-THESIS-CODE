d5d536d3a8e41b3a944fc81492437a383933f356
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-02-18 13:15:37 +0200

    commented some of the experiments so that bigger ones could run

--- scripts/fnn_big_noise.sh
+++ scripts/fnn_big_noise.sh
@@ -3,23 +3,23 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 1000 \
-    --noise-mean 0 \
-    --noise-std 5
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 1000 \
+#     --noise-mean 0 \
+#     --noise-std 5
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 10000 \
-    --noise-mean 0 \
-    --noise-std 5
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 10000 \
+#     --noise-mean 0 \
+#     --noise-std 5
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 100000 \
-    --noise-mean 0 \
-    --noise-std 5
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 100000 \
+#     --noise-mean 0 \
+#     --noise-std 5
 
 singularity run ./containers/torch.sif fnn_experiment.py \
     --processes 10 \
--- scripts/fnn_no_noise.sh
+++ scripts/fnn_no_noise.sh
@@ -3,17 +3,17 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 1000
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 1000
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 10000
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 10000
 
-singularity run ./containers/torch.sif fnn_experiment.py \
-    --processes 10 \
-    --data-set-size 100000
+# singularity run ./containers/torch.sif fnn_experiment.py \
+#     --processes 10 \
+#     --data-set-size 100000
 
 singularity run ./containers/torch.sif fnn_experiment.py \
     --processes 10 \
