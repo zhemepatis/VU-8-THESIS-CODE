0682dbc61c0c0249f40561de52c9b3a1b16ee140
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-03-17 16:54:45 +0200

    added rastrigin function

--- utils/benchmark_funcs.py
+++ utils/benchmark_funcs.py
@@ -7,3 +7,9 @@ class BenchmarkFunctions:
         vector_squared = vector**2
         scalar = np.sum(vector_squared)
         return scalar
+    
+    @staticmethod
+    def rastrigin_func(vector, a = 5):
+        vector_diff = vector**2 - a * np.cos(2 * np.pi * vector)
+        scalar = a * len(vector) + np.sum(vector_diff)
+        return scalar
