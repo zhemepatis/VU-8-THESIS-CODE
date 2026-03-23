9a0e4b6fba2f82ea44e2851427e171e0f40c2175
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-03-23 16:17:23 +0200

    added bar generation script

--- bar_graph.py
+++ bar_graph.py
@@ -6,61 +6,114 @@ import argparse
 if __name__ == "__main__":
 
     # get arguments
-    parser = argparse.ArgumentParser(description = "Graph generator args parser")
+    parser = argparse.ArgumentParser(description = "Bar graph generator args parser")
 
     parser.add_argument(
-        "--data-src",
+        "--data-src-filename",
         type = str,
         required = True,
-        help = "Data file name"
+        help = "Source data filename"
     )
 
     parser.add_argument(
-        "--graph-name",
+        "--graph-filename",
         type = str,
         required = True,
-        help = "Graph picture file name"
+        help = "Graph filename"
+    )
+
+    parser.add_argument(
+        "--graph-title",
+        type = str,
+        required = True,
+        help = "Graph title"
+    )
+
+    parser.add_argument(
+        "--benchmark-func",
+        type = str,
+        default = None,
+        help = "Benchmark function name"
+    )
+
+    parser.add_argument(
+        "--noise-std",
+        type = float,
+        default = None,
+        help = "Gaussian noise std"
     )
 
     args = parser.parse_args()
 
     # get data
-    data_frame = pandas.read_csv(args.data_src)
+    data_frame = pandas.read_csv(args.data_src_filename)
     data_frame.columns = data_frame.columns.str.strip()
     data_frame = data_frame.sort_values(by=["Method", "Data function", "Noise std. deviation", "Data size"]).reset_index(drop = True)
 
+    # prepare labels
     methods = ["1nn", "2nn", "4nn", "8nn", "16nn", "32nn", "fnn"]
     data_sizes = ["1 tūkst.", "10 tūkst.", "100 tūkst.", "1 mln.", "10 mln."]
-    colors = ['cornflowerblue', 'lightsalmon', 'mediumaquamarine', 'plum', 'khaki', 'lightcoral', 'powderblue']
-
+    colors = ["coral", "coral", "coral", "coral", "coral", "coral", "cornflowerblue"]
+    
     x = np.arange(len(data_sizes))
-    width = .5 / len(methods)
+    width = .8 / len(methods)
 
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, method_axis = plt.subplots(figsize=(9, 5))
 
     for i, method in enumerate(methods):
-        filtered = data_frame[(data_frame["Method"] == method) & (data_frame["Noise std. deviation"] == 0)]
-        
+        filtered = data_frame[
+            (data_frame["Data function"] == args.benchmark_func) &
+            (data_frame["Method"] == method) &
+            (data_frame["Noise std. deviation"] == args.noise_std)
+        ]
+
         offset = (i - len(methods) / 2 + 0.5) * width
-        ax.bar(
+        method_axis.bar(
             x + offset,
             filtered["Abs. error mean"].values,
             width = width,
             color = colors[i],
-            alpha = 0.8,
+            edgecolor = "white",
+            alpha = 1,
             label = method
         )
-    
-    ax.set_xticks(x)
-    ax.set_xticklabels(data_sizes)
-    ax.legend()
 
-    ax.set_title("Modelių rezultatai su sferos funkcijos generuotais duomenim")
-    ax.set_xlabel("Taškų kiekis duomenų aibėje")
-    ax.set_ylabel("Abs. paklaidos vidurkis")
+    # primary x-axis configuration   
+    method_ticks = []
+    method_labels = []
+    for xi in x:
+        for i, method in enumerate(methods):
+            offset = (i - len(methods) / 2 + 0.5) * width
+            method_ticks.append(xi + offset)
+            method_labels.append(method)
+
+    method_axis.set_xticks(method_ticks)
+    method_axis.set_xticklabels(method_labels, fontsize = 10, rotation = 90)
+
+    method_axis.set_xticks([tick for tick in method_ticks[::len(data_sizes)]] )
+
+    method_axis.set_xticks(method_ticks)
+    method_axis.set_xticklabels(methods * len(data_sizes), fontsize = 10, rotation = 90)
+    method_axis.tick_params(axis = 'x', length = 0)
+
+    # secondary x-axis configuration
+    data_size_axis = method_axis.twiny()
+
+    data_size_axis.xaxis.set_label_position('bottom')
+    data_size_axis.xaxis.set_ticks_position('bottom')
+
+    data_size_axis.set_xlim(method_axis.get_xlim())
+    data_size_axis.spines['bottom'].set_position(('outward', 35))
+    data_size_axis.set_xticks(x)
+    data_size_axis.set_xticklabels(data_sizes, fontsize = 10)
+
+
+    data_size_axis.set_title(f"{args.graph_title}\n(noise = 0, std = {args.noise_std})", fontsize = 14)
+    data_size_axis.set_xlabel("Taškų kiekis duomenų aibėje", labelpad = 15, fontsize = 12)
     
-    ax.legend()
-    ax.grid(axis = "y", linestyle = "--", alpha = 0.5)
+    # y-axis configuration
+    method_axis.set_ylabel("Abs. paklaidos vidurkis", labelpad = 15, fontsize = 12)
+    method_axis.grid(axis = 'y', linestyle = '--', alpha = 0.5)
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig(args.graph_filename)
--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+BENCHMARK_FUNCS=("sphere_func" "rosenbrock_func" "rastrigin_func")
+TITLES=("Modelių rezultatai su sferos funkcijos generuotais duomenim" "Modelių rezultatai su Rosenbrock'o funkcijos generuotais duomenim" "Modelių rezultatai su Rastrigin'o funkcijos generuotais duomenim")
+
+NOISE_STD_VALUES=(0 0.5 5)
+NOISE_STD_NAMES=("0" "0p5" "5")
+
 mkdir -p "output/graphs"
-python bar_graph.py --data-src "output/raw_data.csv" --graph-name "output/graphs/1nn_0.png"
+
+for i in "${!BENCHMARK_FUNCS[@]}"; do
+    for j in "${!NOISE_STD_VALUES[@]}"; do
+        python bar_graph.py \
+            --data-src-filename "output/raw_data.csv" \
+            --graph-filename "output/graphs/${BENCHMARK_FUNCS[$i]}_${NOISE_STD_NAMES[$j]}.png" \
+            --graph-title "${TITLES[$i]}" \
+            --benchmark-func "${BENCHMARK_FUNCS[$i]}" \
+            --noise-std "${NOISE_STD_VALUES[$j]}"
+    done
+done
