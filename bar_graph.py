9d791dbd065eff8d7d78ceec3ac1f803e0f6b1f0
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-03-23 13:57:42 +0200

    fixed printing issue

--- bar_graph.py
+++ bar_graph.py
@@ -24,42 +24,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # get data
-    print(args.data_src)
-
     data_frame = pandas.read_csv(args.data_src)
-    data_frame = data_frame.sort_values('Data size').reset_index(drop = True)
-
-    data_sizes = data_frame['Data size'].values
-    abs_error_mean = data_frame['Mean'].values
-    std_deviation = data_frame['Std. deviation'].values
-
-    lower = abs_error_mean - std_deviation
-    upper = abs_error_mean + std_deviation
-
-    # generate plots
-    plt.figure()
-
-    plt.plot(
-        data_sizes,
-        abs_error_mean,
-        marker = 'o',
-        label = 'Abs. paklaidos vidurkis'
-    )
-
-    plt.fill_between(
-        data_sizes,
-        lower,
-        upper,
-        alpha = 0.1,
-        label = 'Abs. paklaidos vidurkis ± std. nuokrypis'
-    )
-
-    plt.xlabel('Duomenų aibės dydis')
-    plt.ylabel('Abs. paklaidos vidurkis')
-    plt.title('Dirbtinio neuroninio tinklo rezultatai su sferos funkcijos duomenim\n(sigma + triuksmas)')
-    plt.legend()
+    data_frame = data_frame.sort_values(by = ["Method", "Data function", "Noise std. deviation", "Data size"]).reset_index(drop = True)
     
-    plt.grid()
-    plt.xscale('log')
-
-    plt.show()
+    print(data_frame[data_frame["Method"] == "fnn"])
--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 mkdir -p "output/graphs"
-
-for dataFile in output/raw/sphere_func/*.csv; 
-do python graph_generation.py --data-src $dataFile --graph-name "output/graphs/1nn_0.png";
-done
-
+python bar_graph.py --data-src "output/raw_data.csv" --graph-name "output/graphs/1nn_0.png"
