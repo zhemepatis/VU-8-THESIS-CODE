15ba33a210e50f68f1b50fdaf72c2d2479a6b271
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-05-01 16:27:41 +0300

    some naming changes

--- scripts/combine_csv.sh
+++ scripts/combine_csv.sh
@@ -2,9 +2,6 @@
 
 OUTPUT_FILE="output/raw_data.csv"
 
-# write header to .csv file
-# echo "Method,Data size,Data function,Noise mean,Noise std. deviation,Time elapsed,Abs. error min,Abs. error max,Mean,Abs. error std. deviation" > "$OUTPUT_FILE"
-
 # append data from existing .csv files
 for FILE in output/raw/*.csv; do
     cat "$FILE" >> "$OUTPUT_FILE"
