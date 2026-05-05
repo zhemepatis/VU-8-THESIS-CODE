c1bf7f80efa87ae20e3d68e95e0f8ea623ffc52a
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-05-05 18:45:01 +0300

    added benckmark func names

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -9,6 +9,8 @@ JOB_IDS=()
 
 # setup experiments
 BENCHMARK_FUNCS=(0 1 2)
+BENCHMARK_FUNC_NAMES=("sphere" "rosenbrock" "rastrigin")
+
 DATA_SET_SIZES=(1000 10000 100000 1000000)
 NOISE=("None" 5)
 
@@ -21,8 +23,10 @@ for BENCHMARK_FUNC in "${BENCHMARK_FUNCS[@]}"; do
                 NOISE_STD_INT=$NOISE_STD
             fi 
 
-            JOB_ID=$(sbatch  --parsable scripts/standalone/fnn.sh $BENCHMARK_FUNC $SIZE $NOISE_STD "output/raw/fnn_${NOISE_STD_INT}.csv")
-            echo "[$BENCHMARK_FUNC | size=$SIZE | noise=$NOISE_STD] queued as job $JOB_ID"
+            FUNC_NAME=${BENCHMARK_FUNC_NAMES[$BENCHMARK_FUNC]}
+            
+            JOB_ID=$(sbatch --parsable scripts/standalone/fnn.sh $BENCHMARK_FUNC $SIZE $NOISE_STD "output/raw/fnn_${FUNC_NAME}_${NOISE_STD_INT}.csv")
+            echo "$JOB_ID has been queued"
 
             JOB_IDS+=($JOB_ID)
         done
@@ -39,12 +43,14 @@ for NEIGHBOR_COUNT in "${NEIGHBOR_COUNTS[@]}"; do
         for SIZE in "${DATA_SET_SIZES[@]}"; do
             for NOISE_STD in "${NOISE[@]}"; do
                 NOISE_STD_INT=0
-                if [ NOISE_STD != "None" ]; then
+                if [ $NOISE_STD != "None" ]; then
                     NOISE_STD_INT=$NOISE_STD
                 fi 
 
-                JOB_ID=$(sbatch --parsable scripts/standalone/knn.sh $NEIGHBOR_COUNT $BENCHMARK_FUNC $SIZE $NOISE_STD "output/raw/${NEIGHBOR_COUNT}nn_${NOISE_STD_INT}.csv")
-                echo "[$BENCHMARK_FUNC | size=$SIZE | noise=$NOISE_STD] queued as job $JOB_ID"
+                FUNC_NAME=${BENCHMARK_FUNC_NAMES[$BENCHMARK_FUNC]}
+
+                JOB_ID=$(sbatch --parsable scripts/standalone/knn.sh $NEIGHBOR_COUNT $BENCHMARK_FUNC $SIZE $NOISE_STD "output/raw/${NEIGHBOR_COUNT}nn_${FUNC_NAME}_${NOISE_STD_INT}.csv")
+                echo "$JOB_ID has been queued"
 
                 JOB_IDS+=($JOB_ID)
             done
