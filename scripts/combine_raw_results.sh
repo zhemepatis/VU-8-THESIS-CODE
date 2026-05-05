1a7b2d05bcca5fd62e36dad9bff929b1ca4248be
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-05-05 20:00:13 +0300

    added known rows into combine script

--- scripts/combine_raw_results.sh
+++ scripts/combine_raw_results.sh
@@ -2,6 +2,20 @@
 
 OUTPUT_FILE="${1}"
 
+# add the known rows
+echo "Method,Data size,Data function,Noise mean,Noise std. deviation,Relative error min,Relative error max,Relative error mean,Relative error std. deviation,Time elapsed" \
+  > "$OUTPUT_FILE"
+
+cat << 'EOF' >> "$OUTPUT_FILE"
+fnn,10000000,sphere_func,0,0,0,0,0,0,0
+fnn,10000000,sphere_func,0,5,0,0,0,0,0
+fnn,10000000,rastrigin_func,0,0,0,0,0,0,0
+fnn,10000000,rastrigin_func,0,5,0,0,0,0,0
+fnn,10000000,rosenbrock_func,0,0,0,0,0,0,0
+fnn,10000000,rosenbrock_func,0,5,0,0,0,0,0
+EOF
+
+# add actual results
 for FILE in output/raw/*.csv; do
     cat "$FILE" >> "$OUTPUT_FILE"
 done
