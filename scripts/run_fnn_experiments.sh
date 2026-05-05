479d3f7f9289e79dc47e1ff98e0e3b6e476de4ea
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-05-05 19:43:12 +0300

    some changes to scripts

---
+++ scripts/run_fnn_experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BENCHMARK_FUNCS=(0 1 2)
+BENCHMARK_FUNC_NAMES=("sphere" "rosenbrock" "rastrigin")
+
+DATA_SET_SIZES=(1000 10000 100000 1000000)
+NOISE=("None" 5)
+
+JOB_IDS=()
+for BENCHMARK_FUNC in "${BENCHMARK_FUNCS[@]}"; do
+    for SIZE in "${DATA_SET_SIZES[@]}"; do
+        for NOISE_STD in "${NOISE[@]}"; do
+            NOISE_STD_INT=0
+            if [ $NOISE_STD != "None" ]; then
+                NOISE_STD_INT=$NOISE_STD
+            fi 
+
+            FUNC_NAME=${BENCHMARK_FUNC_NAMES[$BENCHMARK_FUNC]}
+            
+            JOB_ID=$(sbatch --parsable scripts/standalone/fnn.sh $BENCHMARK_FUNC $SIZE $NOISE_STD "output/raw/fnn_${FUNC_NAME}_${NOISE_STD_INT}.csv")
+            echo "$JOB_ID has been queued"
+
+            JOB_IDS+=($JOB_ID)
+        done
+    done
+done
+
+# wait for all fnn jobs to finish
+DEPS=$(IFS=:; echo "${JOB_IDS[*]}")
+sbatch --dependency=afterok:$DEPS
+echo "All fnn jobs are finished"
