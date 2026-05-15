a82403d092a393197f5d1963ed954bd60055ec9a
Gabrielė Rinkevičiūtė <rink.gab@gmail.com>
2026-05-15 15:06:48 +0300

    better readability in combine script

--- scripts/combine_raw_results.sh
+++ scripts/combine_raw_results.sh
@@ -3,10 +3,27 @@
 INPUT_DIR="${1:-output/raw}"
 OUTPUT_FILE="${2:-output/raw_$(date +%Y%m%d_%H%M%S).csv}"
 
-# add the known rows
-echo "Method,Data size,Data function,Noise mean,Noise std. deviation,Absolute error min,Absolute error max,Absolute error mean,Absolute error std. deviation,Relative error min,Relative error max,Relative error mean,Relative error std. deviation,Normalized error min,Normalized error max,Normalized error mean,Normalized error std. deviation,Time elapsed" \
-  > "$OUTPUT_FILE"
+# add header
+echo "Method,\
+Data size,\
+Data function,\
+Noise mean,\
+Noise std. deviation,\
+Absolute error min,\
+Absolute error max,\
+Absolute error mean,\
+Absolute error std. deviation,\
+Relative error min,\
+Relative error max,\
+Relative error mean,\
+Relative error std. deviation,\
+Normalized error min,\
+Normalized error max,\
+Normalized error mean,\
+Normalized error std. deviation,\
+Time elapsed" > "$OUTPUT_FILE"
 
+# add the known rows
 cat << 'EOF' >> "$OUTPUT_FILE"
 fnn,10000000,sphere_func,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 fnn,10000000,sphere_func,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0
